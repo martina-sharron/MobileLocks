beb7842d46e03a2cf7edc13310b1731fb9ee3c2e
Martina Sharron R <martinaraj1209@gmail.com>
2024-12-03 16:06:11 +0530

    commit changes

--- ReduxPin/PinLockSlice.js
+++ ReduxPin/PinLockSlice.js
@@ -1,28 +1,27 @@
-// pinLockSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   pin: '',
   isPinSet: false,
   isError: false,
-};
+}
 
 const pinLockSlice = createSlice({
   name: 'pinLock',
   initialState,
   reducers: {
     setPin(state, action) {
-      state.pin = action.payload;
-      state.isPinSet = true;
+      state.pin = action.payload
+      state.isPinSet = true
     },
     verifyPin(state, action) {
-      state.isError = action.payload !== state.pin;
+      state.isError = action.payload !== state.pin
     },
     resetPin(state) {
-      return initialState;
+      return initialState
     },
   },
-});
+})
 
-export const { setPin, verifyPin, resetPin } = pinLockSlice.actions;
-export default pinLockSlice.reducer;
+export const { setPin, verifyPin, resetPin } = pinLockSlice.actions
+export default pinLockSlice.reducer
